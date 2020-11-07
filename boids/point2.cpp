afcce14502cfcdb550b7cf4425ca9944374898ff
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2020-11-08 02:02:15 +1000

    Fixed formatting

--- boids/point2.cpp
+++ boids/point2.cpp
@@ -1,7 +1,6 @@
 #include "point2.h"
 
-std::ostream& operator<<(std::ostream& os, const Point2& point)
-{
+std::ostream& operator<<(std::ostream& os, const Point2& point) {
 	os << "(" << point.x << ", " << point.y << ")";
 	return os;
 }
