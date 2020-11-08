651e6ebc14f3b81766469a2e6ed032419571493b
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2020-11-08 21:18:16 +1000

    Modified boid print

--- boids/boid.cpp
+++ boids/boid.cpp
@@ -1,6 +1,6 @@
 #include "boid.h"
 
 std::ostream& operator<<(std::ostream& os, const Boid& boid) {
-	os << boid.position.x << "," << boid.position.y << "," << boid.velocity.x << "," << boid.velocity.y;
+	os << "(" << boid.position.x << "," << boid.position.y << "," << boid.velocity.getAngle() << ") ";
 	return os;
 }
