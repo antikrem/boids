af2001ba82fccc878e0e793430c32464b4ac775f
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2020-11-09 22:31:17 +1000

    Final updated parameters

--- boids/boid_settings.cpp
+++ boids/boid_settings.cpp
@@ -1,8 +1,8 @@
 #include "boid_settings.h"
 
-const double BoidSettings::VELOCITY_CAP = 0.5;
-const double BoidSettings::VELOCITY_MIN = 0.3;
-const double BoidSettings::STEER_CAP = 0.2;
+const double BoidSettings::VELOCITY_CAP = 4.0;
+const double BoidSettings::VELOCITY_MIN = 3.0;
+const double BoidSettings::STEER_CAP = 2;
 
 const double BoidSettings::SEPERATION_DISTANCE = 20.0;
 const double BoidSettings::ALIGN_DISTANCE = 50.0;
@@ -11,3 +11,4 @@ const double BoidSettings::COHESION_DISTANCE = 50.0;
 const double BoidSettings::SEPERATION_FACTOR = 1;
 const double BoidSettings::ALIGN_FACTOR = 0.1;
 const double BoidSettings::COHESION_FACTOR = 0.001;
+
